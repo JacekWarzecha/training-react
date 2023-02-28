25cf3ee76246e0e39da4acea31afbf7314d701aa
JacekWarzecha <drjackys@gmail.com>
2023-02-28 18:02:32 +0100

    deleted all, try to fetch

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
-import { useState, useEffect } from "react";
-// import { useLocalStorageState } from "./useLocalStorageState";
-
-// const useDocumentTitle = (counter) => {
-//   useEffect(() => {
-//     document.title = `Licznik: ${counter}`;
-//   }, [counter]);
-// };
-
-// const useHook = () => {
-//   const updateDimensions = () => {
-//     setDimensions({
-//       width: window.innerWidth,
-//       height: window.innerHeight,
-//     });
-//   };
-
-//   const [dimensions, setDimensions] = useState({
-//     width: window.innerWidth,
-//     height: window.innerHeight,
-//   });
-
-//   useEffect(() => {
-//     window.addEventListener("resize", updateDimensions);
-
-//     return () => {
-//       window.removeEventListener("resize", updateDimensions);
-//     };
-//   }, []);
-
-//   return dimensions;
-// };
+// try {
+//   console.log(JSON.parse('{"property: 5}'));
+// } catch (error) {
+//   console.log("nie działa Ci to :");
+// }
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  fetch("products.json")
+    .then((response) => response.json())
+    .then((products) => console.log(products));
 
-  useEffect(() => {
-    setInterval(() => {
-      setCounter((count) => count + 1);
-    }, 1000);
-  }, []);
-
-  return (
-    <div>
-      <p>{counter}</p>
-    </div>
-  );
+  return <div>coś </div>;
 }
 
 export default App;
